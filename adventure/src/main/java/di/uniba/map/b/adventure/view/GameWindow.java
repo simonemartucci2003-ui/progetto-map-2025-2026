e8e94220da6414a0c7ca653678146ecd6c19e8b3
Martucci_Simone <s.martucci11@studenti.uniba.it>
2026-05-16 19:32:54 +0200

    aggiunto view e database

---
+++ adventure/src/main/java/di/uniba/map/b/adventure/view/GameWindow.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package di.uniba.map.b.adventure.view;
+
+/**
+ *
+ * @author simon
+ */
+public class GameWindow {
+    
+}
